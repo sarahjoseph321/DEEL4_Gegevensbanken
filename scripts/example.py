a10dfad7da0ad3b5038792e9b364fef6b5a60d68
eliavw <elia.vw@gmail.com>
2018-03-26 18:29:30 +0200

    Assignment and Prerequisites update

--- scripts/example.py
+++ scripts/example.py
@@ -1,49 +1,19 @@
 """
-This script contains the model solution.
+This is a dummy script of a 'solution' of the first 3 queries
 
 """
+
+
 def query_01(connection, column_names):
-    # Bouw je query
-    query="""
-    select t.name, t.HR
-    from Teams as t;
-    """
-    
-    # Stap 2 & 3
-    res = run_query(connection, query)         # Query uitvoeren
-    df = res_to_df(res, column_names)          # Query in DataFrame brengen
-    
-    return df
+
+    return None
 
 
-def query_02(connection, column_names, datum = '1980-01-16'):
-    # Bouw je query
-    query="""
-    select m.nameFirst, m.nameLast, m.birthYear, m.birthMonth, m.birthDay
-    from Master as m
-    where m.debut > '{}';
-    """.format(datum)
-    
-    # Stap 2 & 3
-    res = run_query(connection, query)         # Query uitvoeren
-    df = res_to_df(res, column_names)          # Query in DataFrame brengen
-    
-    return df
+def query_02(connection, column_names, datum='1980-01-16'):
+
+
+    return None
 
 
 def query_03(connection, column_names):
-    # Bouw je query
-    query = """
-    select distinct m.nameFirst, m.nameLast, t.name
-    from Managers as mgr, Teams as t, Master as m
-    where mgr.playerID = m.playerID
-          and mgr.teamID = t.teamID
-          and t.yearID = mgr.yearID
-          and mgr.plyrMgr = 'N';
-    """
-
-    # Stap 2 & 3
-    res = run_query(connection, query)  # Query uitvoeren
-    df = res_to_df(res, column_names)   # Query in DataFrame brengen
-
-    return df
+    return None
