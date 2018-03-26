eb4532cd10cf39d7d6f4fbc41889c15794ff7f36
eliavw <elia.vw@gmail.com>
2018-03-26 18:32:48 +0200

    Update specs

--- src/specs.py
+++ src/specs.py
@@ -11,13 +11,26 @@ This file contains methods to automatically
 def gen_all_q_names(nb_queries=10):
     """
     Generate list of query names
+
+    :param nb_queries:      Amount of query names to be generated
+    :return:
     """
-    return [gen_q_name(i) for i in range(nb_queries)]
+
+    result = [gen_q_name(i) for i in range(nb_queries)]
+
+    return result
 
 
 def gen_q_name(q_idx):
-    res = "query_{:02d}".format(q_idx+1) # One-based indexing
-    return res
+    """
+    Generate single query name
+
+    :param q_idx:   Index of said query name
+    :return:
+    """
+
+    result = "query_{:02d}".format(q_idx+1) # One-based indexing
+    return result
 
 
 def gen_all_q_method(module, all_q_names):
@@ -26,6 +39,7 @@ def gen_all_q_method(module, all_q_names):
 
     These 'desired methods' are specified by the q_names
     """
+
     all_q_method = {}
     for q_name in all_q_names:
         try:
@@ -34,16 +48,24 @@ def gen_all_q_method(module, all_q_names):
         except BaseException as error:
             method_name = gen_all_q_method.__name__
             print('An exception occurred in method {}:\n{}\n'.format(method_name, error))
+
     return all_q_method
 
 
 def gen_all_q_colnam(all_q_names):
     """
-    Generate a dict containing all the column names of the solution dataframes.
+    Generate a dict containing all the column names of the solution DataFrames.
+
+    This is hardcoded for manifest uniformity, and assumes the standard setting
+    of 10 queries.
+
+    :param all_q_names:     List of query names
+                                E.g. 'query_01'
+    :return:
     """
 
     all_q_colnam = {}
-    all_q_colnam[all_q_names[0]] = ['tname', 'HomeRun']
+    all_q_colnam[all_q_names[0]] = ['tname', 'year','HomeRun']
     all_q_colnam[all_q_names[1]] = ['nameFirst', 'nameLast', 'birthYear', 'birthMonth', 'birthDay']
     all_q_colnam[all_q_names[2]] = ['nameFirst', 'nameLast', 'tname']
     all_q_colnam[all_q_names[3]] = ['tname', 'rank', 'W', 'L', 'nameFirst', 'nameLast']
@@ -82,13 +104,15 @@ def gen_all_q_params(all_q_names = None):
 
     all_q_params[all_q_names[2]] = [q_03_p_01]
 
+    """
+    Nb.: Onderstaande parameterwaarden zijn niet de definitieve!
+    """
+
     # Query 4
     q_04_p_01 = {'datum_x': '1980-01-01',
                  'datum_y': '1980-01-01'}
-    q_04_p_02 = {'datum_x': '2000-01-01',
-                 'datum_y': '2001-01-01'}
 
-    all_q_params[all_q_names[3]] = [q_04_p_01, q_04_p_02]
+    all_q_params[all_q_names[3]] = [q_04_p_01]
 
     # Query 5
     q_05_p_01 = {}
@@ -97,10 +121,8 @@ def gen_all_q_params(all_q_names = None):
 
     # Query 6
     q_06_p_01 = {'salaris': 20000}
-    q_06_p_02 = {'salaris': 40000}
-    q_06_p_03 = {'salaris': 60000}
 
-    all_q_params[all_q_names[5]] = [q_06_p_01, q_06_p_02, q_06_p_03]
+    all_q_params[all_q_names[5]] = [q_06_p_01]
 
     # Query 7
     q_07_p_01 = {}
@@ -110,24 +132,18 @@ def gen_all_q_params(all_q_names = None):
     # Query 8
     q_08_p_01 = {'jaar': 1990,
                  'lengte': 75}
-    q_08_p_02 = {'jaar': 2010,
-                 'lengte': 60}
 
-    all_q_params[all_q_names[7]] = [q_08_p_01, q_08_p_02]
+    all_q_params[all_q_names[7]] = [q_08_p_01]
 
     # Query 9
     q_09_p_01 = {'jaar': 1990}
-    q_09_p_02 = {'jaar': 1995}
-    q_09_p_03 = {'jaar': 2010}
 
-    all_q_params[all_q_names[8]] = [q_09_p_01, q_09_p_02, q_09_p_03]
+    all_q_params[all_q_names[8]] = [q_09_p_01]
 
     # Query 10
     q_10_p_01 = {'jaar': 1968}
-    q_10_p_02 = {'jaar': 1989}
-    q_10_p_03 = {'jaar': 2001}
 
-    all_q_params[all_q_names[9]] = [q_10_p_01, q_10_p_02, q_10_p_03]
+    all_q_params[all_q_names[9]] = [q_10_p_01]
 
     assert len(set(all_q_params)) == len(all_q_names)
 
